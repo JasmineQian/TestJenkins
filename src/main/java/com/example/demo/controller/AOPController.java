c7b50d88ff76d482ad925d75e0e471032f90b339
Jasmine QIAN <jasqia@acxiom.com>
2019-04-25 17:45:01 +0800

    add 2

--- src/main/java/com/example/demo/controller/AOPController.java
+++ src/main/java/com/example/demo/controller/AOPController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.logging.SimpleFormatter;
+
 @RestController
 @RequestMapping
 public class AOPController {
@@ -12,6 +15,7 @@ public class AOPController {
     @RequestMapping(value = "/sayHello",method = RequestMethod.GET)
     @ResponseBody
     public String sayHello(){
-        return "hello Jasmine " + System.currentTimeMillis();
+        SimpleDateFormat simpleFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssSSS");
+        return "hello Jasmine, this time is " + simpleFormatter.format(System.currentTimeMillis());
     }
 }
